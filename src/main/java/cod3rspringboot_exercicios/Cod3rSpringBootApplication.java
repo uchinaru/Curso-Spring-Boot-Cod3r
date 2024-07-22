6f21100747f2b424e30c9dc2f86c295ebf7b6518
Jonatas Benicio <jonathas_santos93@hotmail.com>
2024-07-22 20:24:28 -0300

    init: cria novo projeto em SpringBoot
    Curso para conhecer as diretrizes do Spring

---
+++ src/main/java/cod3rspringboot_exercicios/Cod3rSpringBootApplication.java
@@ -0,0 +1,13 @@
+package cod3rspringboot_exercicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cod3rSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cod3rSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cod3rspringboot_exercicios/Cod3rSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package cod3rspringboot_exercicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cod3rSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
