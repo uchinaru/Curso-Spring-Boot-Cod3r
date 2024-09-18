4e7638f445898760570e61705513422d75a1b43b
Jonatas Benicio <jonathas_santos93@hotmail.com>
2024-09-18 12:50:10 -0300

    feat: metodo para alterar produtos

--- src/main/java/cod3rspringboot_exercicios/controllers/ControllerProduto.java
+++ src/main/java/cod3rspringboot_exercicios/controllers/ControllerProduto.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,5 +55,12 @@ public class ControllerProduto {
 	@GetMapping(path = "/{id}")
 	public Optional<Produto> getProdutoId(@PathVariable int id) {
 		return produtoRep.findById(id);
+	}
+	
+	// Metodo PUT para salvar o objeto por completo, caso queira alterar parcialmente utilize a anotation @PatchMapping
+	@PutMapping
+	public Produto alterarProduto(@Valid Produto produto) {
+		produtoRep.save(produto);
+		return produto;
 	}
 }
